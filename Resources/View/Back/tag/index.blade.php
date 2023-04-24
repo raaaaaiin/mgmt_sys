466356b0ae8351159fc5ec31743062ec713e15b8
Raineer Filosopo <marcraineer0089@gmail.com>
2023-04-24 14:18:13 +0800

    added tag view

---
+++ Resources/View/Back/tag/index.blade.php
@@ -0,0 +1,16 @@
+@extends("back.common.master")
+@section("page_name") {{__("commonv2.tag_mng")}} @endsection
+@section("content")
+    @livewire("tag-mng")
+@endsection
+@section("css_loc")
+    <style>
+
+    </style>
+@endsection
+@section("js_loc")
+    <script>
+
+    </script>
+@endsection
+
