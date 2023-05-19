681a921e76218823268465b29c07f2b83f1e3bfd
raaaaaiin <marcraineer0089@gmail.com>
2023-05-19 11:09:27 +0800

    Added FontAwesome

--- Resources/View/Back/author-mng.php
+++ Resources/View/Back/author-mng.php
@@ -1,122 +1,108 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <script type="application/javascript" src="PreRequisites/jQuery_v3.6.0.js"></script>
+    <link rel="stylesheet" href="Resources/CSS/adminlte.css">
+    <link rel="stylesheet" href="Resources/CSS/fontawesome-free/css/all.min.css">
+
+    </style>
+</head>
+
 <div>
-    @php
-        /* @var \App\Facades\Util $util */
-        /* @var \App\Facades\Common $common */
-    @endphp
     <div class="card">
-        @include("back.common.spinner")
+        <!-- Placeholder for spinner -->
         <div class="card-body yellow">
-            @if(session()->has("form_author") && session()->has("form_author"))
-                <div class="row">
-                    <div class="col-12">
-                        @include("common.messages")
-                    </div>
+            <!-- If session has "form_author" and "form_author" -->
+            <div class="row">
+                <div class="col-12">
+                    <!-- Include common messages -->
                 </div>
-            @endif
+            </div>
 
             <form class="col-12" role="form" wire:submit.prevent="saveAuthor">
                 <div class="form-row">
-
                     <div class="col-12 mb-2">
                         <div class="input-group">
-                            <div class="input-group-prepend"><span
-                                    class="input-group-text">{{__("common.author_name")}}</span></div>
+                            <div class="input-group-prepend"><span class="input-group-text">{{__("common.author_name")}}</span></div>
                             <div class="form-control border-0 p-0">
-                                <input type="text" class="form-control" required
-                                       wire:model.defer="author_name">
+                                <input type="text" class="form-control" required wire:model.defer="author_name">
                             </div>
                             <div class="input-group-append">
-                                
+                                <!-- Additional append content -->
                             </div>
                         </div>
-                        <div
-                            class="w-100">@error('author_name') @include('back.common.validation', ['message' =>  $message ]) @enderror</div>
+                        <div class="w-100"><!-- @error('author_name') @include('back.common.validation', ['message' =>  $message ]) @enderror --></div>
                     </div>
                 </div>
             </form>
             <div class="col-12 mt-2 mb-2">
-                <div class="d-block">
-                    {!! CForm::inputGroupHeader(__("common.filter")) !!}
+                <div class="d-flex">
+                    <!-- Placeholder for input group header -->
                     <input type="text" class="form-control" wire:model.debounce.800ms="search_keyword">
                     <div class="input-group-append">
                         <button class="btn btn-danger" type="button" wire:click="clearSearch()">
                             <i class="far fa-trash-alt"></i>
                         </button>
-                        <button type="submit"
-                                        class="btn btn-sm btn-dark">
-                                    @if($mode=="create")
-                                        <i class="fas fa-plus-circle"></i>
-                                        {{__("common.create")}}
-                                    @else
-                                        <i class="fas fa-save"></i>
-                                        {{__("common.update")}}
-                                    @endif
-                                    {{__("common.author")}}
-                                </button>
+                        <button type="submit" class="btn btn-sm btn-dark">
+                            <!-- If $mode == "create" -->
+                            <!-- else -->
+                            <!-- endif -->
+                            {{__("common.author")}}
+                        </button>
                     </div>
-                    {!! CForm::inputGroupFooter() !!}
+                    <!-- Placeholder for input group footer -->
                 </div>
                 <div class="table-responsive mt-2">
                     <table class="table table-hover table-sm">
                         <thead>
-                        <tr>
-                            <th scope="col">{{__("common.id")}}</th>
-                            <th scope="col">{{__("common.author_name")}}</th>
-                            <th scope="col">{{__("commonv2.books_written")}}</th>
-                            <th scope="col">{{__("common.action")}}</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">{{__("common.id")}}</th>
+                                <th scope="col">{{__("common.author_name")}}</th>
+                                <th scope="col">{{__("commonv2.books_written")}}</th>
+                                <th scope="col">{{__("common.action")}}</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        @if($items->total())
-
-                            @foreach($items as $item)
-                                <tr>
-                                    <td>{{$item->id}}</td>
-                                    <td>{{$item->name}}</td>
-                                    <td>
-                                        @php $books_titles = $item->books()->pluck("books.title")->toArray(); @endphp
-                                        @if(count($books_titles))
-                                            @foreach($books_titles as $book)
-                                                <a target="_blank"
-                                                   href="{{url('/')."/details/".$common::utf8Slug($book)}}"
-                                                   class="btn-link text-sm">{{Str::title($book)}}</a>
-                                                @if(!$loop->last),@endif
-                                            @endforeach
-                                        @else
-                                            <span class="text-sm">--</span>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <button type="button" wire:click="editAuthor({{$item->id}})"
-                                                class="btn float-left btn-sm btn-dark action_btn">
-                                            <i class="far fa-edit"></i>
-                                        </button>
-                                        <button
-                                            wire:loading.class="disabled"
-                                            onclick="lv_confirm_then_submit(this,'{{__("common.cnf_del")}}','deleteAuthor','{\'id\':{{$item->id}}}')"
-                                            type="button"
-                                            class="btn float-left btn-sm btn-danger action_btn">
-                                            <i class="fas fa-trash-alt"></i>
-                                        </button>
-
-                                    </td>
-
-                                </tr>
-                            @endforeach
-                        @else
+                            <!-- If $items->total() -->
+                            <!-- foreach $items as $item -->
+                            <tr>
+                                <td>Data</td>
+                                <td><!-- $item->name --></td>
+                                <td>
+                                    <!-- Placeholder for books_titles -->
+                                    <!-- If count($books_titles) -->
+                                    <!-- foreach $books_titles as $book -->
+                                    <a target="_blank" href="<!-- url('/') -->/details/<!-- $common::utf8Slug($book) -->" class="btn-link text-sm"><!-- Str::title($book) --></a>
+                                    <!-- if not $loop->last -->,<!-- endif -->
+                                    <!-- endforeach -->
+                                    <!-- else -->
+                                    <span class="text-sm">--</span>
+                                    <!-- endif -->
+                                </td>
+                                <td>
+                                    <button type="button" class="btn float-left btn-sm btn-dark action_btn">
+                                        <i class="far fa-edit"></i>
+                                    </button>
+                                    <button type="button" class="btn float-left btn-sm btn-danger action_btn">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                            <!-- endforeach -->
+                            <!-- else -->
                             <tr>
                                 <td colspan="10">
                                     <div class="alert alert-dark">{{__("commonv2.no_author_exist")}}</div>
                                 </td>
                             </tr>
-                        @endif
+                            <!-- endif -->
                         </tbody>
                     </table>
                 </div>
-                {{$items->links()}}
+                <!-- Placeholder for pagination links -->
             </div>
         </div>
     </div>
 </div>
-
-
