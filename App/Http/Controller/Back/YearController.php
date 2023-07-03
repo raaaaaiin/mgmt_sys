b1fc7ee59c7637927a711934a2c42accf6cd81b8
raaaaaiin <marcraineer0089@gmail.com>
2023-07-03 09:54:47 +0800

    Added few data and adjusted model

--- App/Http/Controller/Back/YearController.php
+++ App/Http/Controller/Back/YearController.php
@@ -1,11 +1,17 @@
 <?php
 namespace App\Http\Controller\Back;
+
+use App\Models\Year;
+use App\Common\Model;
+
 class YearController{
     public function YearController(): void{
         $this->render();
     }
     function render(): void
     {
+        $yearList = (new Model())->table('year')->get();
+        var_dump($yearList);
         require_once 'Resources\View\Back\year\index.php';
     }
 
