5c1bc0979d06fffa243936bca1e9c2d2bd46f91a
raaaaaiin <marcraineer0089@gmail.com>
2023-07-17 10:19:18 +0800

    added authentication

--- App/Common/Authentication.php
+++ App/Common/Authentication.php
@@ -3,6 +3,28 @@
 namespace App\Common;
 
 class Authentication{
+    public static $options;
+    public function __construct() {
+        $options = [
+            'cost' => 10, 
+        ];
+        
+    }
+    
+    public static function validate($request){
+        return self::compare($request);
+    }
+
+    public static function compare($data): bool {
+        return password_verify($data['pass'], $data);
+        
+    }
+
+    public static function make($args){
+        return password_hash($args,PASSWORD_BCRYPT,  self::$options);
+    }
+
+
 }
 
 ?>
