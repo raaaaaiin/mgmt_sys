ba97a2b2f6b1e076e7786ba95e700254eb3144a2
raaaaaiin <marcraineer0089@gmail.com>
2023-07-17 08:23:03 +0800

    Added Session

---
+++ App/Common/Session.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Common;
+
+class Session
+{
+    
+    
+    private static int $id;
+
+    private static string $name;
+    private static string $section;
+    private static string $yearLevel;
+    private static string $location;
+
+    public static function initiate()
+    {
+   }
+    public static function getID(){
+
+    }
+    public static function getName(){
+
+    }
+    public static function getSection(){
+
+    }
+    public static function getYearLevel(){
+
+    }
+    public static function getLocation(){
+
+    }
+    public static function getAboutMe(){
+
+    }
+    public static function getDue(){
+
+    }
+
+}
